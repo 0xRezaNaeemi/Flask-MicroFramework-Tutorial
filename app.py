6cf35f8238c3c6b8d0e646b5662937f687705068
Reza Naeemi <RezaNaeemiRad@gmail.com>
2023-10-23 14:55:25 +0330

    send data from url to templates as a variable

--- app.py
+++ app.py
@@ -7,8 +7,12 @@ def index():
     context = [1, 2, 3, 4, 5]
     return render_template('index.html', my_list=context)
 
+@app.route('/<name>/')
+def welcome(name):
+    return render_template('welcome.html', name=name)
+
 """
-1. create a list and pass to render_template
-2. use 'for' from jinja templates language in index.html
+1. pass name variable from url to templates
+2. use name in index.html
 """
 
