f2ba7ee7dab04fdd17f1bb2d9d40c210cbc5ceea
Reza Naeemi <RezaNaeemiRad@gmail.com>
2023-10-25 00:44:12 +0330

    Check File format filter

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ os.makedirs(path, exist_ok=True)
 
 app = Flask(__name__)
 
+allowed_formats = {"txt", "jpg", "png"}
+def checkFileFormat(fileName):
+    return "." in fileName and fileName.rsplit(".", 1)[1] in allowed_formats
+
 @app.route('/')
 def index():
     return render_template('form.html')
@@ -23,17 +27,27 @@ def showResult():
 def upload():
     return render_template('upload.html')
 
+
 @app.route('/uploaded/', methods=['POST'])
 def savefile():
+    
     theFile = request.files['file']
-    if theFile.filename != "":
+
+    if checkFileFormat(theFile.filename):
         try:
-            goal_path = theFile.save(os.path.join(path, theFile.filename))
+            goal_path = os.path.join(path, theFile.filename)
             theFile.save(goal_path)
-            return "your file has been saved successfully"
-        except Exception as e:
-            return e
+            return "your file has been saved successfully."
+        except Exception:
+            return "oh! something is wrong."
     else:
-        return "the file name is invalid"
+        return "Your file format is not allowed."
 
 
+
+"""
+Check File format filter:
+
+1. create checkFileFormat() function (line 11)
+2. call checkFileFormat() function (line 36)
+"""
