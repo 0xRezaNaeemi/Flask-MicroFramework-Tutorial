6d74db00511c3f946c567abc00bf505d401ff9a4
Reza Naeemi <RezaNaeemiRad@gmail.com>
2023-10-26 00:17:13 +0330

    session is better than cookies

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 
 import os
-from flask import Flask, render_template, request, make_response
+from flask import Flask, render_template, request, make_response, session
 
 app = Flask(__name__)
 
+app.secret_key = "aaaaaaadasdasfsadfsdafsdfsda"
 
 @app.route('/')
 def home():
-    if request.cookies.get("user_email"):
-        return f"welcome your email is: {request.cookies['user_email']}"
+    if session.get("user_email"):
+        return f"welcome your email is: {session['user_email']}"
     else:
         return render_template('form.html')
 
@@ -18,12 +19,9 @@ def submitForm():
     try:
         email = request.form["email"]
         # password = request.form["password"]
-
-        # request.cookies["email"] = "asdf" # this dic is immuteable 
-        # 
-        response = make_response("Successful <a href='/'> HOME </a>")
-        response.set_cookie("user_email", email)
-        return response
+         
+        session["user_email"] = email
+        return "Successful <a href='/'> HOME </a>"
     except Exception as e:
         return f"sorry not successful the error was: {e}"
 
@@ -31,9 +29,8 @@ def submitForm():
 """
 cookies:
 
-1.import request, make_response 
-2. no need to import cookies because cookies are in request
-2. we can not use request.cookies["email"] = "asdf" because Dictionary is imuteable
-3.  make_response convert returned responses to client, so we can add cookies to responses by make_response function
-4. create "if" statement in home def for checking cookies
+1.import session
+2. add secret_key for app (line 7)
+2. recode cookies to session 
+
 """
