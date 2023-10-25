ec28144cdebd2257583ae40945d4350659dee051
Reza Naeemi <RezaNaeemiRad@gmail.com>
2023-10-26 00:28:25 +0330

    session expired date

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 
-import os
+from datetime import timedelta
 from flask import Flask, render_template, request, make_response, session
 
 app = Flask(__name__)
 
 app.secret_key = "aaaaaaadasdasfsadfsdafsdfsda"
+app.permanent_session_lifetime = timedelta(days=1)
+
+
 
 @app.route('/')
 def home():
@@ -21,16 +24,19 @@ def submitForm():
         # password = request.form["password"]
          
         session["user_email"] = email
+        session.permanent = True
+    
+        
         return "Successful <a href='/'> HOME </a>"
     except Exception as e:
         return f"sorry not successful the error was: {e}"
 
 
 """
-cookies:
+session expired date:
 
-1.import session
-2. add secret_key for app (line 7)
-2. recode cookies to session 
+1. from datetime import timedelta
+2. session.permanent = True
+3. app.permanent_session_lifetime = timedelta(days=1)
 
 """
