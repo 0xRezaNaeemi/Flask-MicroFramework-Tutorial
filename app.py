61ea133aec909595bd81e76b5fc67065b7b3e39c
Reza Naeemi <RezaNaeemiRad@gmail.com>
2023-10-26 00:58:08 +0330

    API

--- app.py
+++ app.py
@@ -1,42 +1,23 @@
 
-from datetime import timedelta
-from flask import Flask, render_template, request, make_response, session
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-app.secret_key = "aaaaaaadasdasfsadfsdafsdfsda"
-app.permanent_session_lifetime = timedelta(days=1)
-
-
-
 @app.route('/')
 def home():
-    if session.get("user_email"):
-        return f"welcome your email is: {session['user_email']}"
-    else:
-        return render_template('form.html')
+    return "Home page"
 
 
-@app.route("/submit", methods=["POST"])
-def submitForm():
-    try:
-        email = request.form["email"]
-        # password = request.form["password"]
-         
-        session["user_email"] = email
-        session.permanent = True
-    
+@app.route("/api")
+def handleAPI():
+    my_dictionary = {
+        "Products":[
+            {"name": "iphone", "price": 2588520, "stock": True},
+            {"name": "samsung", "price": 25884520, "stock": False},
+            {"name": "huawei", "price": 258520, "stock": True},
+            {"name": "realme", "price": 15487514, "stock": False},
+        ]
+    }
         
-        return "Successful <a href='/'> HOME </a>"
-    except Exception as e:
-        return f"sorry not successful the error was: {e}"
+    return my_dictionary
 
-
-"""
-session expired date:
-
-1. from datetime import timedelta
-2. session.permanent = True
-3. app.permanent_session_lifetime = timedelta(days=1)
-
-"""
