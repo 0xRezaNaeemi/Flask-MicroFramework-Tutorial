5b52437ab0fbbda6f19599855400e11338a8d91f
Reza Naeemi <RezaNaeemiRad@gmail.com>
2023-10-25 16:04:26 +0330

    Error 404 handling

--- app.py
+++ app.py
@@ -1,20 +1,35 @@
 
 import os
-from flask import Flask , render_template, request, redirect, url_for
+from flask import Flask, render_template, abort
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    return redirect(url_for('redirectToThisFunction'))
-
-@app.route('/upload/')
-def redirectToThisFunction():
     return render_template('upload.html')
 
-"""
-redirect and url_for:
+@app.route('/form')
+def form():
+    return render_template('form.html')
 
-1. import redirect and url_for from flask module
-2. call redirect and url_for (line 9)
+@app.route('/404')
+def error404():
+    # abort(503)
+    abort(404)
+
+@app.errorhandler(404)
+def showError(error):
+    # return "The error is " + str(error)
+    return render_template("404.html"), 404
+
+
+"""
+error 404 handling:
+
+1. import abort from flask module
+2. use abort (line 16-24)
+3. create 404.html
+4. type mispelled route in url 
+    http://127.0.0.1:5000/formm
 """
