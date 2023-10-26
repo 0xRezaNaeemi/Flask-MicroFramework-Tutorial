aaa4f069fa6f2fc3579fdd3e2535bde697e74380
Reza Naeemi <RezaNaeemiRad@gmail.com>
2023-10-26 13:58:48 +0330

    create a database using SQLAlchemy

--- app.py
+++ app.py
@@ -1,23 +1,38 @@
 
-from flask import Flask, render_template
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
 
+path = os.path.dirname(__file__)
+db_dir = os.path.join(path, "app.db")
+
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + db_dir
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+
+# use one time when creating a database
+# with app.app_context():
+#     db.create_all()
+
 @app.route('/')
 def home():
-    return "Home page"
+    return db_dir
 
+"""
+create database:
 
-@app.route("/api")
-def handleAPI():
-    my_dictionary = {
-        "Products":[
-            {"name": "iphone", "price": 2588520, "stock": True},
-            {"name": "samsung", "price": 25884520, "stock": False},
-            {"name": "huawei", "price": 258520, "stock": True},
-            {"name": "realme", "price": 15487514, "stock": False},
-        ]
-    }
-        
-    return my_dictionary
+1. pip install Flask-SQLAlchemy 
+2. from flask_sqlalchemy import SQLAlchemy
+3. app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + db_dir
+4. creating a database use this function
+    with app.app_context():
+        db.create_all()
 
+"""
